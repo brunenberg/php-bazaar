ac180127947e7d84ca4d494133ded2d99fcb0a65
Delluci <wmn.brunenberg@student.avans.nl>
2024-03-16 00:22:20 +0100

    feat(Translate)

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -13,7 +13,7 @@
 </div>
 
 <div class="mx-20 mt-5 p-5 rounded bg-[{{$company->background_color}}] text-[{{$company->text_color}}]">
-    <h2 class="font-bold text-2xl pb-3">Reviews</h2>
+    <h2 class="font-bold text-2xl pb-3">{!!__('Reviews')!!}</h2>
     <div class="grid grid-cols-4">
         @foreach ($company->reviews as $review)
             <div class="flex flex-col justify-between items-center border-b pb-3 mb-3 bg-white rounded mr-5 text-black">
@@ -28,7 +28,7 @@
                     <form action="{{route('company/delete-review')}}" method="POST">
                         @csrf
                         <input type="hidden" name="company_id" value="{{$company->id}}">
-                        <button type="submit" class="text-red-500 hover:text-red-700">Delete</button>
+                        <button type="submit" class="text-red-500 hover:text-red-700">{!!__('Delete')!!}</button>
                     </form>
                 @endif
             </div>
@@ -38,13 +38,13 @@
 
 @if (auth()->check() && auth()->user()->user_type === 'gebruiker')
     <div class="mx-20 mt-5 p-5 rounded bg-[{{$company->background_color}}] text-[{{$company->text_color}}]">
-        <h2 class="font-bold text-2xl">Schrijf een review</h2>
+        <h2 class="font-bold text-2xl">{!!__('Write a review')!!}</h2>
         <form action="{{route('company/review')}}" method="POST">
             @csrf
             <input type="hidden" name="company_id" value="{{$company->id}}">
             <div class="flex flex-col gap-4">
                 <div>
-                    <label for="rating" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating</label>
+                    <label for="rating" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{!!__('Rating')!!}</label>
                     <select id="rating" name="rating" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -54,10 +54,10 @@
                     </select>
                 </div>
                 <div>
-                    <label for="review" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Review</label>
+                    <label for="review" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{!!__('Review')!!}</label>
                     <textarea id="review" name="review" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></textarea>
                 </div>
-                <button type="submit" class="bg-blue-500 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Plaatsen</button>
+                <button type="submit" class="bg-blue-500 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{!!__('Post')!!}</button>
             </div>
     </div>
 @endif
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -20,7 +20,7 @@
 </div>
 
 <div class="mx-36 mt-20 bg-white rounded-lg shadow-md p-6">
-    <h2 class="font-bold text-2xl pb-3">Reviews</h2>
+    <h2 class="font-bold text-2xl pb-3">{!!__('Reviews')!!}</h2>
     <div class="grid grid-cols-4">
         @foreach ($listing->reviews as $review)
             <div class="flex flex-col justify-between items-center border-b pb-3 mb-3 bg-white rounded mr-5">
@@ -35,7 +35,7 @@
                     <form action="{{route('listing/delete-review')}}" method="POST">
                         @csrf
                         <input type="hidden" name="listing_id" value="{{$listing->id}}">
-                        <button type="submit" class="text-red-500 hover:text-red-700">Delete</button>
+                        <button type="submit" class="text-red-500 hover:text-red-700">{!!__('Delete')!!}</button>
                     </form>
                 @endif
             </div>
@@ -44,20 +44,20 @@
 </div>
 
 <div class="mx-36 mt-20 bg-white rounded-lg shadow-md p-6">
-    <h2 class="font-bold text-2xl pb-3">Deel deze advertentie</h2>
+    <h2 class="font-bold text-2xl pb-3">{!!__('Share this listing')!!}</h2>
     <img src="{{$qrCode}}" alt="QR Code">
 </div>
 
 
 @if (auth()->check() && auth()->user()->user_type === 'gebruiker')
 <div class="mx-36 mt-20 bg-white rounded-lg shadow-md p-6">
-    <h2 class="font-bold text-2xl pb-3">Review Plaatsen</h2>
+    <h2 class="font-bold text-2xl pb-3">{!!__('Post a review')!!}</h2>
     <form action="{{route('listing/review')}}" method="POST">
         @csrf
         <input type="hidden" name="listing_id" value="{{$listing->id}}">
         <div class="flex flex-col gap-4">
             <div>
-                <label for="rating" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating</label>
+                <label for="rating" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{!!__('Rating')!!}</label>
                 <select id="rating" name="rating" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -67,10 +67,10 @@
                 </select>
             </div>
             <div>
-                <label for="review" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Review</label>
+                <label for="review" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{!!__('Review')!!}</label>
                 <textarea id="review" name="review" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></textarea>
             </div>
-            <button type="submit" class="bg-blue-500 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Plaatsen</button>
+            <button type="submit" class="bg-blue-500 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{!!__('Post')!!}</button>
         </div>
 </div>
 @endif
